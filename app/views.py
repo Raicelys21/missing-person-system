263818fd9bd95c7d56d6f8a946513174b1bbf753
Raicelys21 <raicelys21@hotmail.com>
2022-11-14 14:23:24 -0400

    last

--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ import os, glob
 from cgi import print_arguments
 import cv2
 import face_recognition
-
+from PIL import Image
 
 def addElement(request):
     if request.method == "POST":
@@ -27,55 +27,60 @@ def compareElement(request2):
     images2 = Identify.objects.all()
     return render(request2, 'compare.html', {'images2': images2, 'form2': form2})
 
+def delete_all(request4):
+    Identify.objects.all().delete()
+    dir = KNOWN_IMAGES_FOLDER_PATH2
+    filelist = glob.glob(os.path.join(dir, "*"))
+    for f in filelist:
+        os.remove(f)
+    form2 = IdentifyForm()
+    images2 = Identify.objects.all()
 
+    return render(request4, 'compare.html', {'images2': images2, 'form2': form2})
+
+
+KNOWN_IMAGES_FOLDER_PATH2 = "C:/Users/Raicelys Suero/Documents/CST-10/Prog. Web Avanz/Missing Person System - Copy/missingperson/media/identify"
 KNOWN_IMAGES_FOLDER_PATH = "C:/Users/Raicelys Suero/Documents/CST-10/Prog. Web Avanz/Missing Person System/missingperson/media/gallery"
-KNOWN_IMAGES_FOLDER_PATH2 = "C:/Users/Raicelys Suero/Documents/CST-10/Prog. Web Avanz/Missing Person System/missingperson/media/identify"
 
+def executeCompare(request3)-> tuple[str, str]:
 
-def executeCompare()-> tuple[str, str]:
-    image_files = os.listdir(KNOWN_IMAGES_FOLDER_PATH)
-    images_path: list[str] = []
-
+    #Identify
     image_files2 = os.listdir(KNOWN_IMAGES_FOLDER_PATH2)
     images_path2: list[str] = []
+
+    for file2 in image_files2:
+        images_path2.append(f"{KNOWN_IMAGES_FOLDER_PATH2}/{file2}")
+
+    #Gallery
+    image_files = os.listdir(KNOWN_IMAGES_FOLDER_PATH)
+    images_path: list[str] = []
 
     for file in image_files:
         images_path.append(f"{KNOWN_IMAGES_FOLDER_PATH}/{file}")
 
-    for file in image_files2:
-        images_path2.append(f"{KNOWN_IMAGES_FOLDER_PATH2}/{file}")
 
-    print(images_path)
-    print(images_path2)
-    
-    ReadImage1 = cv2.imread(images_path[0])
-    ReadImage1 = cv2.cvtColor(ReadImage1, cv2.COLOR_BGR2RGB)
-    ReadImage1_encoding = face_recognition.face_encodings(ReadImage1)[0]
+    for x in images_path:
+        ReadImage1 = cv2.imread(x)
+        ReadImage1 = cv2.cvtColor(ReadImage1, cv2.COLOR_BGR2RGB)
+        ReadImage1_encoding = face_recognition.face_encodings(ReadImage1)[0]
 
-    ReadImage2 = cv2.imread(images_path2[0])
-    ReadImage2 = cv2.cvtColor(ReadImage2, cv2.COLOR_BGR2RGB)
-    ReadImage2_encoding = face_recognition.face_encodings(ReadImage2)[0]
+        for y in images_path2:
+            ReadImage2 = cv2.imread(y)
+            ReadImage2 = cv2.cvtColor(ReadImage2, cv2.COLOR_BGR2RGB)
+            ReadImage2_encoding = face_recognition.face_encodings(ReadImage2)[0]
 
-    Resultado = face_recognition.compare_faces(
-        [ReadImage1_encoding], ReadImage2_encoding)
-    print("Resultado: ", Resultado)
+            Resultado = face_recognition.compare_faces([ReadImage1_encoding], ReadImage2_encoding)
+            print(Resultado)
 
-    cv2.imshow('img', ReadImage1)
-    cv2.waitKey(0)
-    cv2.imshow('img2', ReadImage2)
+            if Resultado == [True]:
+                url_image = x 
+                print(url_image)
+                image = Image.open(url_image)
+                image.show()
     
     form2 = IdentifyForm()
     images2 = Identify.objects.all()
-    return Resultado, render('compare.html', {'images2': images2, 'form2': form2})
+
+    return render(request3, 'compare.html', {'images2': images2, 'form2': form2})
 
 
-def delete_all(request4):
-    Identify.objects.all().delete()
-    dir = 'C:/Users/Raicelys Suero/Documents/CST-10/Prog. Web Avanz/Missing Person System/missingperson/media/identify'
-    filelist = glob.glob(os.path.join(dir, "*"))
-    for f in filelist:
-        os.remove(f)
-
-    form2 = IdentifyForm()
-    images2 = Identify.objects.all()
-    return render(request4, 'compare.html', {'images2': images2, 'form2': form2})
